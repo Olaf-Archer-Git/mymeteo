b1732477d6a1dfe13b1cddb3e88bd1d565701239
olto <olto@gmail.com>
2022-07-05 17:36:35 +0300

    develop-footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
           GitHub
         </a>
       </div>
-      <p className="footer__text">2022 &copy; Ukraine. olto@protonmail.com</p>
+      <p className="footer__text">Oleg Todoriko. olto@protonmail.com</p>
       <div className="footer__link">
         <a
           href="https://www.linkedin.com/in/oleh-todoriko-506699231/"
